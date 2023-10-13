28f41396b32362a8511ae9eca046d166dd17c8bf
bilguun erdem <uuganchimegenhtulga@gmail.com>
2023-10-14 01:21:05 +0800

    hehe

---
+++ javascript.js
@@ -0,0 +1,22 @@
+console.log("where am i");
+var name = "mlbb-hahahhahhsahhah";
+console.log("minoin"+name+"gedeg. ");
+var age, nas;
+age = 24;
+nas = 23;
+console.log("your age is : "+ nas);
+
+var too;
+console.log(too);
+var number = null;
+console.log(number);
+var birthYear, nowAge =23, currentYear = 2023;
+birthDay = currentYear -nowAge;
+console.log("tanii torsn on bol " + birthYear);
+// tanii 10 jiliin daraah nas : <...>
+
+var hi, hu, hy;
+hi = 8;
+hu = 2;
+hy = 3;
+console.log(hi + hu +hy);
